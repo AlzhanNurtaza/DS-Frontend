a6cc8c10ba3c7137a670c80c8d255f2102f64151
alzhan.nurtaza <alzhan.nurtaza@gmail.com>
2024-01-24 20:32:14 +0600

    UI: mainFilters added

--- src/pages/performances/performance.tsx
+++ src/pages/performances/performance.tsx
@@ -1,8 +1,37 @@
 import React from 'react'
-import { Typography } from 'antd';
+import { Col, DatePicker, Row, Space, Switch, Typography, theme } from 'antd';
+import locale from 'antd/es/date-picker/locale/ru_RU';
+
+const {Text} = Typography; 
+const { useToken } = theme;
+
+const topColStyle = {
+  xs: 24,
+  sm:24,
+  md:8
+}
+
 
 export const Performance: React.FC = () => {
+  const { token } = useToken();
+
   return (
-    <Typography>performance</Typography>
+    <Row gutter={24} style={{ display: 'flex'}}>
+      <Col {...topColStyle} >
+      <Space direction="horizontal">
+        <Text style={{color:token.colorWhite}}>Без доли</Text>
+        <Switch />
+        <Text style={{color:token.colorWhite}}>Дата</Text>
+        <DatePicker locale={locale}/>
+        </Space>
+      </Col>
+      <Col {...topColStyle}>
+        ExchangeRate
+      </Col>
+      <Col {...topColStyle}>
+        ExchangeRate
+      </Col>
+    </Row>
+    
   )
 }
