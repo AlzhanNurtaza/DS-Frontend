4312a12fc92c68ae032c2d045595ba2e0e38b149
alzhan.nurtaza <alzhan.nurtaza@gmail.com>
2024-04-09 15:11:32 +0500

    UI: login endpoint changed

--- src/authProvider.ts
+++ src/authProvider.ts
@@ -8,6 +8,31 @@ import axios from "axios";
 export const axiosInstance = axios.create();
 const strapiAuthHelper = AuthHelper(API_URL + "/api");
 
+interface ILoginResponse {
+    jwt: string;
+    user: IUser;
+}
+
+interface IRole {
+    id: number | string;
+    name: string;
+    description: string;
+    type: string;
+}
+
+interface IUser {
+    id: number | string;
+    username: string;
+    email: string;
+    provider: string;
+    confirmed: boolean;
+    blocked: boolean;
+    role: IRole;
+    created_at: string;
+    updated_at: string;
+}
+
+
 export const authProvider: AuthBindings = {
     onError: async (error) => {
         if (error.status === 401 || error.status === 403) {
@@ -22,7 +47,11 @@ export const authProvider: AuthBindings = {
         },
 
     login: async ({ email, password }) => {
-        const { data, status } = await strapiAuthHelper.login(email, password);
+        //const { data, status } = await strapiAuthHelper.login(email, password);
+        const { data, status } = await axios.post<ILoginResponse>(API_URL + "/api/auth/login", {
+            email,
+            password,
+        });
         if (status === 200) {
             localStorage.setItem(TOKEN_KEY, data.jwt);
 
